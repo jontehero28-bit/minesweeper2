82588774c1361c9dad7fa15d3b0386f41ff0a851
Yuriy Petrov <Jurijp2005@gmail.com>
2023-01-29 21:13:44 +0100

    commit4

--- minesweeper2.js
+++ minesweeper2.js
@@ -17,11 +17,25 @@ window.onload = function()    //ska genomföra det direkt efter sidan laddas kla
 
 function setMines()          //ställer ut miner och jag skriver id där minerna ska vara
 {
-    minesLocation.push("0-0");
+    /*minesLocation.push("0-0");
     minesLocation.push("0-1");
     minesLocation.push("3-5");
-    minesLocation.push("1-2");
-    
+    minesLocation.push("1-2");*/
+
+    let minesLeft = minesCount; //skapar en random number generator
+    while (minesLeft > 0) 
+    {
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        let id = r.toString() + "-" + c.toString();
+
+        if (!minesLocation.includes(id))
+        {
+            minesLocation.push(id);
+            minesLeft -= 1; //det finns en chans att en id för en bomb blir skapad två ggr därför jag ändrar den genom -1
+        }
+    }
+        
 }
 
 function startGame() //startar de funktioner när spelet börjas
@@ -64,6 +78,11 @@ function setFlag() {
 
 function clickTile ()
 {
+    if (gameOver || this.classList.contains("tile-clicked"))//ifall det är true 
+    {
+        return;
+    } //den skickar något tillbaka och kommer inte göra allt som står nedanför
+
     let tile = this; //referar till rutan som blev tryckt
     if (flagEnabled)
     {
@@ -78,7 +97,7 @@ function clickTile ()
         return;
     }
 
-    if (minesLocation.includes(tile.id))
+    if (minesLocation.includes(tile.id))//ifall minesLocation har tile.id true då har man gått på en bomb och då är det game over. Förklarar hur det funkar längre ner
     {
         //alert("GAME OVER");
         gameOver = true;
@@ -111,12 +130,20 @@ function revealMines()//gör så att man ser miner
     
 }
 
-function checkMine(r, c)
+function checkMine(r, c) //ska checka igenom miner
 {
+    
     if(r < 0 || r >= rows || c < 0 || c >= columns)
     {
         return;
     }
+    if(board[r][c].classList.contains("tile-clicked"))
+    {
+        return;
+    }
+
+    board[r][c].classList.add("tile-clicked"); //ändrar färg på rutan som är redan tryckt. Den plockar klassen från style.css
+    tilesClicked += 1; //när man trycker på en ruta utan en bomb så höjs den med 1. När alla rutor är tryckta så vinner man.
 
     let minesFound = 0; //antal hittade miner     /*jag ska gå igenom rutorna runt om bomben. Jag chechar rutor runt de*/ 
 
@@ -134,21 +161,44 @@ function checkMine(r, c)
     minesFound += checkTile(r+1, c); //bottom 
     minesFound += checkTile(r+1, c+1); //bottom right
 
-    if(minesFound > 0)
+    if(minesFound > 0) //ifall antal bomber är större än 0
     {
-        board[r][c].innerText = minesFound;
+        board[r][c].innerText = minesFound; //då ritar upp siffror 1, 2, 3 osv.
         board[r][c].classList.add("b" + minesFound.toString()); //ändrar färg på siffran beroende på hur många bomber det  är. Tar det från style.css dokument
+    }
+    else //ifall man hittade inga miner i närheten så ska rutan fråga andra rutor runt om kring att checka ifall det finns miner nära de.
+    {
+        //top 3
+        checkMine(r-1, c-1); //top left
+        checkMine(r-1, c);  //top
+        checkMine(r-1, c+1); //top right
+
+        //left and right
+        checkMine(r, c-1); //left
+        checkMine(r, c+1); //right
+        
+        //bottom
+        checkMine(r+1, c-1); //bottom left
+        checkMine(r+1, c);  //bottom
+        checkMine(r+1, c+1); //bottom right
+
+    }
+
+    if (tilesClicked == rows * columns - minesCount)//antal rutor är rows gånger columns -antal miner
+    { //när alla rutor är trycka så vinner man
+        document.getElementById("mine-count").innerText = "Cleared"
+        gameOver = true;
     }
 }
 
-function checkTile(r, c)
+function checkTile(r, c) //checkar rutor genom deras id.
 {
-    if(r < 0 || r >= rows || c < 0 || c >= columns)
+    if(r < 0 || r >= rows || c < 0 || c >= columns) //ifall man hittar ingenting då returnera värde 0.
     {
         return 0;
     }
 
-    if (minesLocation.includes(r.toString() + "-" + c.toString()))
+    if (minesLocation.includes(r.toString() + "-" + c.toString())) //ifall man hittade en bomb nära returnera värde 1
     {
         return 1;
     }
